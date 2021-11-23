661a4e5dd7666ffede7148ecf264625cd5c86a19
mastaleru <mastaleru.rafael@gmail.com>
2021-11-23 17:53:58 +0200

    added navigation to econsent from first page

--- code/scripts/controllers/general/LandingController.js
+++ code/scripts/controllers/general/LandingController.js
@@ -1,10 +1,24 @@
 const {WebcController} = WebCardinal.controllers;
+const ecoServices = require('eco-services');
+const DIDService = ecoServices.DIDService;
 const usecases = WebCardinal.USECASES;
 
 export default class LandingController extends WebcController {
     constructor(...props) {
         super(...props);
         this.model = JSON.parse(JSON.stringify(usecases));
+        this.addHandlers();
+        this.initServices();
+    }
+
+    addHandlers(){
+        this.onTagEvent("navigate:econsent-home","click",()=>{
+            this.navigateToPageTag('econsent-home');
+        })
+    }
+
+    async initServices(){
+        this.CommunicationService = await DIDService.getCommunicationServiceInstanceAsync(this);
     }
 
 }
