7e2698a0fea70fc3d4cc07f24a18768d349a0156
mastaleru <mastaleru.rafael@gmail.com>
2021-11-25 12:01:48 +0200

    refactoring and cleanup

---
+++ code/scripts/controllers/econsent/TrialsDashboard.js
@@ -0,0 +1,104 @@
+import TrialService from '../../services/TrialService.js';
+import TrialConsentService from '../../services/TrialConsentService.js';
+
+const {WebcController} = WebCardinal.controllers;
+
+const ecoServices = require('eco-services');
+const DIDService = ecoServices.DIDService;
+const BaseRepository = ecoServices.BaseRepository;
+
+export default class TrialsDashboard extends WebcController {
+    constructor(...props) {
+        super(...props);
+        this.setModel({});
+        this._initServices();
+        this._initHandlers();
+        this._initTrials();
+        this._initTrialParticipant();
+    }
+
+    async _initServices() {
+        this.TrialService = new TrialService();
+        this.TrialParticipantRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.TRIAL_PARTICIPANT);
+        this.NotificationsRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.NOTIFICATIONS);
+        this.EconsentsStatusRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.ECOSESENT_STATUSES);
+        this.VisitsAndProceduresRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.VISITS);
+        this.QuestionsRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.QUESTIONS);
+
+        this.CommunicationService = await DIDService.getCommunicationServiceInstanceAsync(this);
+        this.TrialConsentService = new TrialConsentService();
+        this.model.trialConsent = await this.TrialConsentService.getOrCreateAsync();
+    }
+
+    _initHandlers() {
+        this._attachHandlerTrialClick();
+        this._attachHandlerSites();
+        this._attachHandlerVisits();
+        this._attachHandlerQuestions();
+    }
+
+    _initTrials() {
+        this.model.trials = [];
+        this.TrialService.getTrials((err, data) => {
+            if (err) {
+                return console.error(err);
+            }
+            this.model.trials = data;
+        });
+    }
+
+
+    //TO BE REMOVED/REFACTORED
+    _initTrialParticipant() {
+        this.model.tp = {};
+        this.TrialParticipantRepository.findAll((err, data) => {
+            if (err) {
+                return console.log(err);
+            }
+            if (data && data.length > 0) {
+                this.model.tp = data[data.length - 1];
+            }
+        });
+    }
+
+
+    _attachHandlerTrialClick() {
+        this.onTagEvent('home:trial', 'click', (trial, target, event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+
+            this.navigateToPageTag('trial', {
+                trialSSI: trial.keySSI,
+                tpDid: this.model.tp.did,
+                isNewTp: this.model.isNewTp,
+            });
+        });
+    }
+
+    _attachHandlerSites() {
+        this.onTagClick('home:site', (event) => {
+            this.navigateToPageTag('site');
+        });
+    }
+
+    _attachHandlerQuestions() {
+        this.onTagClick('home:questionns', (event) => {
+            this.navigateToPageTag('questions');
+        });
+    }
+
+    _attachHandlerVisits() {
+        this.onTagClick('home:visits', (event) => {
+            this.navigateToPageTag('visits-procedures', {
+                tpDid: this.model.tp.did,
+                tpUid: this.model.tp.uid
+            });
+        });
+    }
+
+
+
+
+
+
+}
