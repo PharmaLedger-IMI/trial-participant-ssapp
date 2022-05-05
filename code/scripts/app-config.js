40d3ebe7d9e96880078847e1e8d1cc5fc34ff49f
Cosmin Tuca <tuc@rms.ro>
2022-05-05 14:44:09 +0300

    Integrate share-did component #176

--- code/scripts/app-config.js
+++ code/scripts/app-config.js
@@ -21,3 +21,14 @@ addHook('beforeAppLoads', async () => {
         console.error('Error while defining WebCardinal components', error);
     }
 });
+
+addHook('beforeAppLoads', async () => {
+    try {
+        defineWebCardinalComponents();
+
+        await import("../components/share-did/share-did.js");
+
+    } catch (error) {
+        console.error('Error while defining WebCardinal components', error);
+    }
+});
