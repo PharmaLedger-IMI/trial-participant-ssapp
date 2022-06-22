253d730aa4f0da8e30d2d6124651da6f7f03e8f4
nliappas <67898132+nliappas@users.noreply.github.com>
2022-06-22 11:01:14 +0200

    update DP to stop sharing

--- code/scripts/controllers/iot/ViewStudyDetailsController.js
+++ code/scripts/controllers/iot/ViewStudyDetailsController.js
@@ -31,7 +31,37 @@ export default class ViewStudyDetailsController extends WebcController {
                     console.log(data);
                     console.log("DPermission added!");
                     this.CommunicationService.sendMessageToIotAdapter({
-                        operation: "dp_updated",
+                        operation: "dp_updated_add",
+                        studyUID: this.model.study.uid,
+                        dpUID: data.uid
+                    })
+                })
+            });
+            this.navigateToPageTag('home')
+        });
+
+        this.onTagClick('stop-sharing', (model) => {
+            this.CommunicationService = getCommunicationServiceInstance();
+            this.dpservice = DPService.getDPService();
+            this.dpservice.getDPs((err, DPs) => {
+                if (err) {
+                    return console.log(err);
+                }
+                let DP = DPs && DPs.length > 0 ? DPs[0] : undefined
+                if((DP.matches) && (DP.matches.length>0)) {
+                    let studyIndex = DP.matches.findIndex(match => match.study.uid === this.model.study.uid);
+                    DP.matches[studyIndex].dpermission = false;
+                    DP.matches[studyIndex].dpermissionStopSharingDate = new Date();
+                    DP.matches[studyIndex].study.StopSharingDate = new Date();
+                }
+                this.dpservice.updateDP(DP, (err, data) => {
+                    if (err){
+                        console.log(err);
+                    }
+                    console.log(data);
+                    console.log("DPermission removed!");
+                    this.CommunicationService.sendMessageToIotAdapter({
+                        operation: "dp_updated_remove",
                         studyUID: this.model.study.uid,
                         dpUID: data.uid
                     })
