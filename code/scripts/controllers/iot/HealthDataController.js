6ff22370efe0bb80ba302541717a044b33e4a707
Rasel Ahmed <raselahmed.lucse@gmail.com>
2022-07-12 12:42:23 +0100

    Health Data Display: Not all the health data is displayed + various improvements #239

--- code/scripts/controllers/iot/HealthDataController.js
+++ code/scripts/controllers/iot/HealthDataController.js
@@ -10,22 +10,24 @@ export default class HealthDataController extends WebcController {
         // this.initServices();
         const prevState = this.getState() || {};
         console.log(prevState)
-        var data =  prevState[0];
+        var data =  prevState;
+        var countHasValue = data.length - 1;
         console.log("Health Controller");
         console.log(data);
        this.model.healthData = [];
+       this.model.hasValue = data[countHasValue];
 
-       if (prevState[1]) {
-           this.model.hasValue = true;
-       }
-       else {
-           this.model.hasValue = false;
-       }
+    //    if (prevState[1]) {
+    //        this.model.hasValue = true;
+    //    }
+    //    else {
+    //        this.model.hasValue = false;
+    //    }
 
         //this.model.hasValue = prevState[1].hasValue;
         console.log(this.model.hasValue);
-        if(data.length>0){
-            for(let i=0; i<data.length; i++){
+        if(countHasValue>0){
+            for(let i=0; i<countHasValue; i++){
                 let data1 = data[i];
                 let fullDateTime1 = data1.effectiveDateTime;
                 let dateTime1 =  fullDateTime1.split("T");
--- code/scripts/controllers/iot/SelectHealthDataController.js
+++ code/scripts/controllers/iot/SelectHealthDataController.js
@@ -22,27 +22,53 @@ export default class SelectHealthDataController extends WebcController {
                 if(err){
                     console.log(err);
                 }
-                var pageValue = [];
-                console.log(data);
-                if(data.length>0){
-                    let countData = data.length - 1;
-                    var result = data[countData];
-                    var selected = this.model.selectObservation.value;
-                    console.log(selected)
-                    console.log(this.model.selectObservation.value);
-                    let tempVal =  result.filter(o => o.code.text.includes(selected));
-                    // console.log("This is the Temp Value");
-                    // console.log(tempVal);
-                    pageValue.push(tempVal);
-                    pageValue.push({hasValue:true});
+                // var pageValue = [];
+                // console.log(data);
+                // if(data.length>0){
+                //     let countData = data.length - 1;
+                //     var result = data[countData];
+                //     var selected = this.model.selectObservation.value;
+                //     console.log(selected)
+                //     console.log(this.model.selectObservation.value);
+                //     let tempVal =  result.filter(o => o.code.text.includes(selected));
+                //     // console.log("This is the Temp Value");
+                //     // console.log(tempVal);
+                //     pageValue.push(tempVal);
+                //     pageValue.push({hasValue:true});
 
+                // }
+                var pageValue = [];
+                console.log("*************** View IoT Data ***************")
+                console.log(data);
+                
+                if(data.length){
+                    // let countData = data.length - 1;
+                    for(let i=1; i<data.length;  i++){
+                        var result = data[i];
+                        // console.log("************* Result: **********");
+                        // console.log(result);
+                        var selected = this.model.selectObservation.value;
+                        let tempVal =  result.filter(o => o.code.text.includes(selected));
+                        // console.log("************* Temp Value: **********");
+                        // console.log(tempVal);
+                        for(let j=0; j<tempVal.length; j++){
+                            pageValue.push(tempVal[j]);
+                        }
+                    }
+                    if(pageValue.length){
+                        pageValue.push({hasValue:true});
+                    }
+                    else {
+                        pageValue.push({hasValue:false});
+                    }
+                    
                 }
                 else {
                     
                     pageValue.push({hasValue:false});
                     console.log("Didn't find any value!");
                 }
-                
+                console.log(pageValue);
                 this.navigateToPageTag('iot-helath-data', pageValue);
 
             });
