a95481cccf32f7d24873ced8ab24b2bcfbfd8bb0
mastaleru <mastaleru.rafael@gmail.com>
2022-07-13 12:18:46 +0300

    #239

--- code/scripts/controllers/iot/HealthDataController.js
+++ code/scripts/controllers/iot/HealthDataController.js
@@ -1,49 +1,32 @@
 const {WebcController} = WebCardinal.controllers;
 const commonServices = require('common-services');
-const HealthDataService = commonServices.HealthDataService;
-
 
 
 export default class HealthDataController extends WebcController {
     constructor(...props) {
         super(...props);
         // this.initServices();
-        const prevState = this.getState() || {};
-        console.log(prevState)
-        var data =  prevState;
-        var countHasValue = data.length - 1;
+        const data = this.getState() || {};
         console.log("Health Controller");
         console.log(data);
        this.model.healthData = [];
-       this.model.hasValue = data[countHasValue];
+       this.model.hasHealthData = data.length>0;
 
-    //    if (prevState[1]) {
-    //        this.model.hasValue = true;
-    //    }
-    //    else {
-    //        this.model.hasValue = false;
-    //    }
 
-        //this.model.hasValue = prevState[1].hasValue;
-        console.log(this.model.hasValue);
-        if(countHasValue>0){
-            for(let i=0; i<countHasValue; i++){
-                let data1 = data[i];
-                let fullDateTime1 = data1.effectiveDateTime;
-                let dateTime1 =  fullDateTime1.split("T");
-                let time1 = dateTime1[1].split(".");
-                this.model.healthData.push({
-                    title: data1.code.text,
-                    value: data1.valueQuantity.value,
-                    unit: data1.valueQuantity.unit,
-                    date: dateTime1[0],
-                    time: time1[0]
-                });
-            }
-            
-            console.log(this.model.healthData);
+        for (let i = 0; i < data.length; i++) {
+            let data1 = data[i];
+            let fullDateTime1 = data1.effectiveDateTime;
+            let dateTime1 = fullDateTime1.split("T");
+            let time1 = dateTime1[1].split(".");
+            this.model.healthData.push({
+                title: data1.code.text,
+                value: data1.valueQuantity.value,
+                unit: data1.valueQuantity.unit,
+                date: dateTime1[0],
+                time: time1[0]
+            });
         }
-        
+
         this._attachHandlerGoBack();
     }
     _attachHandlerGoBack() {
--- code/scripts/controllers/iot/SelectHealthDataController.js
+++ code/scripts/controllers/iot/SelectHealthDataController.js
@@ -7,69 +7,28 @@ const HealthDataService = commonServices.HealthDataService;
 export default class SelectHealthDataController extends WebcController {
     constructor(...props) {
         super(...props);
-        // this.initServices();
-        // options = []
-        // this.healthDataService.getAllObservations((err, data)=>{
-        //     var result = data[0];
-            
-        // });
 
         this.healthDataService = new HealthDataService();
-        //this.model.header = "Select IoT Health Data";
         this.model = this.getSelectHealthDataViewModel();
         this.onTagClick("view-iot-data",()=>{
             this.healthDataService.getAllObservations((err, data)=>{
                 if(err){
                     console.log(err);
                 }
-                // var pageValue = [];
-                // console.log(data);
-                // if(data.length>0){
-                //     let countData = data.length - 1;
-                //     var result = data[countData];
-                //     var selected = this.model.selectObservation.value;
-                //     console.log(selected)
-                //     console.log(this.model.selectObservation.value);
-                //     let tempVal =  result.filter(o => o.code.text.includes(selected));
-                //     // console.log("This is the Temp Value");
-                //     // console.log(tempVal);
-                //     pageValue.push(tempVal);
-                //     pageValue.push({hasValue:true});
 
-                // }
-                var pageValue = [];
+                data = [].concat(...data);
+
                 console.log("*************** View IoT Data ***************")
                 console.log(data);
-                
-                if(data.length){
-                    // let countData = data.length - 1;
-                    for(let i=1; i<data.length;  i++){
-                        var result = data[i];
-                        // console.log("************* Result: **********");
-                        // console.log(result);
-                        var selected = this.model.selectObservation.value;
-                        let tempVal =  result.filter(o => o.code.text.includes(selected));
-                        // console.log("************* Temp Value: **********");
-                        // console.log(tempVal);
-                        for(let j=0; j<tempVal.length; j++){
-                            pageValue.push(tempVal[j]);
-                        }
-                    }
-                    if(pageValue.length){
-                        pageValue.push({hasValue:true});
-                    }
-                    else {
-                        pageValue.push({hasValue:false});
-                    }
-                    
-                }
-                else {
-                    
-                    pageValue.push({hasValue:false});
-                    console.log("Didn't find any value!");
-                }
-                console.log(pageValue);
-                this.navigateToPageTag('iot-helath-data', pageValue);
+
+
+                let selectedObservation = this.model.selectObservation.value;
+                let desiredHealthData = data.filter(observation=>{
+                    return observation.code.text === selectedObservation
+                })
+
+                console.log(desiredHealthData);
+                this.navigateToPageTag('iot-helath-data', desiredHealthData);
 
             });
             
