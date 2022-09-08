e0221335f72fb878ce4724936d9bce713fae12a8
Cosmin Tuca <tuc@rms.ro>
2022-09-08 15:30:46 +0300

    add statuses for other scheduled visits https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/446

--- code/scripts/controllers/econsent/VisitDetailsController.js
+++ code/scripts/controllers/econsent/VisitDetailsController.js
@@ -13,12 +13,17 @@ export default class VisitDetailsController extends WebcController {
         this.model.details = this.state.details;
 
         this.state.visits.forEach(visit=>{
-            if (typeof visit.accepted === "undefined" && typeof visit.declined === "undefined" && typeof visit.rescheduled === "undefined" && typeof visit.confirmed==='undefined') {
-                visit.pending = true;
+            if(visit.accepted === true || visit.confirmed === true) {
+                return visit.status = 'accepted';
             }
-        })
+            if(visit.rescheduled === true) {
+                return visit.status = 'rescheduled';
+            }
+            return visit.status = 'pending';
+        });
+
         this.state.visits = this.state.visits.filter((visit) => {
-            return visit.uid !== this.state.uid
+            return visit.uid !== this.state.uid && !visit.declined
         })
 
         this.model.visits = this.state.visits.sort((a, b) => a.proposedDate - b.proposedDate);
