daebd6d664542d2cfbd0ceab7256795a6ddd7879
mastaleru <mastaleru.rafael@gmail.com>
2022-09-26 20:35:28 +0300

    fixes for https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/397

--- code/scripts/controllers/econsent/TaskCalendarController.js
+++ code/scripts/controllers/econsent/TaskCalendarController.js
@@ -232,10 +232,8 @@ export default class TaskCalendarController extends WebcController {
     sendMessageToHCO(visit, message) {
         this.CommunicationService.sendMessage(this.model.tp.hcoIdentity, {
             operation: Constants.MESSAGES.HCO.COMMUNICATION.TYPE.VISIT_RESPONSE,
-            ssi: visit.trialSSI,
             useCaseSpecifics: {
                 tpDid: this.model.tp.did,
-                trialSSI: visit.trialSSI,
                 visit: {
                     details: visit.details,
                     toRemember: visit.toRemember,
