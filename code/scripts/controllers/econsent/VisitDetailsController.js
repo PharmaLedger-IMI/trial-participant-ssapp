633074249cc188865d231f162ff222b7cb19ee79
Cosmin Tuca <tuc@rms.ro>
2022-09-30 11:00:59 +0300

    reschedule btn from visit details https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/497

--- code/scripts/controllers/econsent/VisitDetailsController.js
+++ code/scripts/controllers/econsent/VisitDetailsController.js
@@ -1,7 +1,11 @@
+import {getTPService} from "../../services/TPService.js";
+
 const {WebcController} = WebCardinal.controllers;
 const commonServices = require("common-services");
 const Constants = commonServices.Constants;
 const momentService = commonServices.momentService;
+const BaseRepository = commonServices.BaseRepository;
+const CommunicationService = commonServices.CommunicationService;
 
 export default class VisitDetailsController extends WebcController {
     constructor(...props) {
@@ -11,6 +15,7 @@ export default class VisitDetailsController extends WebcController {
         };
 
         this.model.details = this.state.details;
+        this.CommunicationService = CommunicationService.getCommunicationServiceInstance();
 
         this.state.visits.forEach(visit=>{
             if(visit.accepted === true || visit.confirmed === true) {
@@ -31,12 +36,69 @@ export default class VisitDetailsController extends WebcController {
         if(this.state.schedule.startDate) {
             this.model.toShowDate = momentService(this.state.schedule.startDate).format(Constants.DATE_UTILS.FORMATS.DateTimeFormatPattern);
         }
+        this.getTp();
         this._initHandlers();
     }
 
     _initHandlers() {
         this._attachHandlerBack();
         this._attachHandlerProcedures();
+        this._attachHandlerRescheduleVisit();
+    }
+
+    getTp() {
+        this.TPService = getTPService();
+        this.TPService.getTp((err, tp) => {
+            if (err) {
+                return console.log(err);
+            }
+            this.model.tp = tp;
+        })
+    }
+
+    _attachHandlerRescheduleVisit() {
+        this.onTagEvent('reschedule-visit', 'click', (model, target, event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            const VisitsAndProceduresRepository = BaseRepository.getInstance(BaseRepository.identities.PATIENT.VISITS);
+
+            VisitsAndProceduresRepository.findBy(this.state.uid, (err, existingVisit) => {
+                if (err || !existingVisit) {
+                    return console.err(err);
+
+                }
+                this.showModalFromTemplate(
+                    'econsent/reschedule-invitation',
+                    async (event) => {
+                        const response = event.detail.desiredDate;
+                        if(response) {
+                            let date = new Date(response);
+                            existingVisit.rescheduled = true;
+                            existingVisit.proposedDate = date.getTime();
+                            if(existingVisit.confirmedDate) {
+                                delete existingVisit.confirmedDate;
+                                delete existingVisit.accepted;
+                                delete existingVisit.confirmed;
+                            }
+                            await VisitsAndProceduresRepository.updateAsync(existingVisit.pk, existingVisit)
+                            this.sendMessageToHCO(existingVisit, Constants.MESSAGES.HCO.COMMUNICATION.PATIENT.VISIT_RESCHEDULED);
+                        }
+                    },
+                    (event) => {
+                        const response = event.detail;
+                    },
+                    {
+                        controller: 'modals/RescheduleInvitationController',
+                        disableExpanding: false,
+                        disableBackdropClosing: true,
+                        question: 'Are you sure you want to reschedule this visit?',
+                        title: 'Reschedule visit',
+                        suggestedInterval: existingVisit.suggestedInterval,
+                        isExtended: existingVisit.isExtended
+                    }
+                );
+            });
+        });
     }
 
     _attachHandlerBack() {
@@ -58,4 +120,22 @@ export default class VisitDetailsController extends WebcController {
             this.navigateToPageTag('procedures-view', info);
         });
     }
+
+    sendMessageToHCO(visit, message) {
+        this.CommunicationService.sendMessage(this.model.tp.hcoIdentity, {
+            operation: Constants.MESSAGES.HCO.COMMUNICATION.TYPE.VISIT_RESPONSE,
+            useCaseSpecifics: {
+                tpDid: this.model.tp.did,
+                visit: {
+                    procedures: visit.procedures,
+                    name: visit.name,
+                    rescheduled: visit.rescheduled,
+                    proposedDate: visit.proposedDate,
+                    id: visit.uid
+                },
+            },
+            shortDescription: message,
+        });
+    }
+
 }
